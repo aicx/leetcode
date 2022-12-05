ca41991020263aa893b188f927e20007bd3d2917
guangxing.xu <ting.liu@freemud.com>
2022-12-05 11:23:02 +0800

    init...

---
+++ src/main/java/com/leetcode/a1/Solution.java
@@ -0,0 +1,29 @@
+package com.leetcode.a1;
+
+/**
+ 示例 1：
+
+ 输入：nums = [2,7,11,15], target = 9
+ 输出：[0,1]
+ 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ 示例 2：
+
+ 输入：nums = [3,2,4], target = 6
+ 输出：[1,2]
+ 示例 3：
+
+ 输入：nums = [3,3], target = 6
+ 输出：[0,1]
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        int temp = 0;
+        for (int num : nums) {
+            temp = num;
+        }
+        return null;
+    }
+}
